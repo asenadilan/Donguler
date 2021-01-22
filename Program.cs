91e617c5b23a05db70db2f54f684c8d8aa3fae13
asenadilan <37346720+asenadilan@users.noreply.github.com>
2021-01-22 19:50:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım geliştirme kampı";
+            string kurs2 ="Yazılım başlangıç kursu ";
+            string kurs3 = "java";
+
+            string[] kurslar = new string[] { "Yazılım geliştirme kampı" , "Yazılım başlangıç kursu", "java"};
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+
+            }
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+        }
+    }
+}
